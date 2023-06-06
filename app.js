434024a39d083f7eb27f3cb12215987dce3d2cf7
Waldemar Szymon <walldy@tlen.pl>
2023-06-06 23:37:29 +0200

    Modify addImageWatermarkToImage function

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   await image.quality(100).writeAsync(outputFile);
 };
 
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+  await image.quality(100).writeAsync(outputFile);
+};
+
 addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world');
+addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
